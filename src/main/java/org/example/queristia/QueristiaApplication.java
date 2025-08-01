00ab6c0f182926187a8fec62fc1196ab8502bf9e
kimseokjun <k03s13j@naver.com>
2025-08-01 14:01:03 +0900

    first commit

---
+++ src/main/java/org/example/queristia/QueristiaApplication.java
@@ -0,0 +1,13 @@
+package org.example.queristia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueristiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueristiaApplication.class, args);
+    }
+
+}
